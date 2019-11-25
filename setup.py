8014060098717d10dd4d72bb0744cc3fa2d11a79
torklo <tord.kloster@knowit.no>
2019-11-25 08:55:33 +0100

    add all packages, exclude tests

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.oslo.kommune.no/origo-dataplatform/origo-sdk-python",
-    packages=["origo"],
+    packages=setuptools.find_packages(".", exclude=["tests*"]),
     install_requires=[
         "requests",
         "urllib3",
