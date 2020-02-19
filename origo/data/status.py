9d191fbdf108c4609d7c1e8e9d73b471b4ab97ca
Kristoffer Strekerud <kristr@knowit.no>
2020-02-19 12:53:03 +0100

    DP-375: Making get

--- origo/data/status.py
+++ origo/data/status.py
@@ -14,5 +14,13 @@ class Status(SDK):
         self.__name__ = "status"
         super().__init__(config, auth)
 
-    def get_status(self):
-        return "status"
+    def get_status(self, uuid):
+        url = self.config.get("statusApiUrl")
+        log.info(f"Retrieving status for UUID={uuid} from: {url}")
+
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return response.content
+        else:
+            log.info(f"Was unable to retrieve status for UUID={uuid} from: {url}")
