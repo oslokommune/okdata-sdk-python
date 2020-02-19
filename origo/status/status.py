030897e6cdc23b2e0c37d370923400d15378a70e
Kristoffer Strekerud <kristr@knowit.no>
2020-02-19 14:06:36 +0100

    DP-375: Raising exception on not found

--- origo/status/status.py
+++ origo/status/status.py
@@ -14,10 +14,11 @@ class Status(SDK):
     def get_status(self, uuid):
         url = self.config.get("statusApiUrl")
         log.info(f"Retrieving status for UUID={uuid} from: {url}")
-
         response = requests.get(url + uuid)
-
-        if response.status_code == 200:
-            return response.text
+        print(f"response.status_code: {response.status_code}")
+        if response.status_code == 200 and response.json():
+            return response.json()
         else:
-            log.info(f"Was unable to retrieve status for UUID={uuid} from: {url}")
+            log.info(
+                f"Was unable to retrieve status for UUID={uuid} from: {url}")
+            raise KeyError(response.status_code, uuid)
