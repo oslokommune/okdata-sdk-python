8740e1e6bf28c026fdd49f589d2349caa53af197
Petter H. Juliussen <petter.juliussen@byr.oslo.kommune.no>
2020-08-25 13:21:14 +0200

    DP-933: Update methods for event stream resource

--- origo/event/event_stream_client.py
+++ origo/event/event_stream_client.py
@@ -8,19 +8,19 @@ class EventStreamClient(SDK):
     def __init__(self, config=None, auth=None, env=None):
         self.__name__ = "event_stream_client"
         super().__init__(config, auth, env)
-        self.stream_manager_url = self.config.get("streamManagerUrl")
+        self.event_stream_api_url = self.config.get("eventStreamApiUrl")
 
-    def create_event_stream(self, dataset_id, version):
-        response = self.post(self.event_stream_url(dataset_id, version), data=None)
+    def create_event_stream(self, dataset_id, version, create_raw=True):
+        response = self.post(
+            f"{self.event_stream_api_url}/{dataset_id}/{version}",
+            data={"create_raw": create_raw},
+        )
         return response.json()
 
     def get_event_stream_info(self, dataset_id, version):
-        response = self.get(self.event_stream_url(dataset_id, version))
+        response = self.get(f"{self.event_stream_api_url}/{dataset_id}/{version}")
         return response.json()
 
     def delete_event_stream(self, dataset_id, version):
-        response = self.delete(self.event_stream_url(dataset_id, version))
+        response = self.delete(f"{self.event_stream_api_url}/{dataset_id}/{version}")
         return response.json()
-
-    def event_stream_url(self, dataset_id, version):
-        return f"{self.stream_manager_url}/stream/{dataset_id}/{version}"
