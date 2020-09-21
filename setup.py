224b8b1ba04ab38aec3eba710f58ad1eccaae561
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2020-09-21 15:42:35 +0200

    Bump version: 0.2.5 → 0.2.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="origo-sdk-python",
-    version="0.2.5",
+    version="0.2.6",
     author="Oslo Origo",
     author_email="dataplattform@oslo.kommune.no",
     description="SDK for origo",
