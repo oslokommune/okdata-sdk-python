36e846a25f37c0fd2a38ad196117eefb51f1ada1
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2021-04-27 14:19:38 +0200

    DP-1944 Restore Python 3.6 support

--- okdata/sdk/permission/user_types.py
+++ okdata/sdk/permission/user_types.py
@@ -1,19 +1,52 @@
-from dataclasses import dataclass, field
+# TODO: Use these simplified dataclasses once support for Python 3.6 is
+# dropped. Meanwhile we'll use the "polyfill" classes defined below.
+#
+# from dataclasses import dataclass, field
+#
+# @dataclass
+# class Client:
+#     user_id: str
+#     user_type: str = field(default="client", init=False)
+#
+#
+# @dataclass
+# class Team:
+#     user_id: str
+#     user_type: str = field(default="team", init=False)
+#
+#
+# @dataclass
+# class User:
+#     user_id: str
+#     user_type: str = field(default="user", init=False)
 
 
-@dataclass
 class Client:
     user_id: str
-    user_type: str = field(default="client", init=False)
+    user_type: str
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.user_type = "client"
 
 
-@dataclass
 class Team:
     user_id: str
-    user_type: str = field(default="team", init=False)
+    user_type: str
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.user_type = "team"
 
 
-@dataclass
 class User:
     user_id: str
-    user_type: str = field(default="user", init=False)
+    user_type: str
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.user_type = "user"
+
+
+def asdict(obj):
+    return obj.__dict__
