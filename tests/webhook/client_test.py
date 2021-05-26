522fb0d5c3b8d3461400dceb30522e094af283eb
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2021-05-26 12:24:05 +0200

    DP-2085 Handle webhook tokens based on operation
    Create and authorize webhook tokens based on operations (read, write)
    instead of tying them to a specific service.

--- tests/webhook/client_test.py
+++ tests/webhook/client_test.py
@@ -7,17 +7,17 @@ from okdata.sdk.webhook.client import WebhookClient
 def test_create_webhook_token(requests_mock):
     client = WebhookClient()
     dataset_id = "dataset-id"
-    service_name = "some-service"
+    operation = "read"
     matcher = re.compile(f"{client.api_url}/{dataset_id}/tokens")
 
     def body_matcher(request):
-        return request.json() == {"service": service_name}
+        return request.json() == {"operation": operation}
 
     res = {
         "token": "774d8f35-fb4b-4e06-9d7f-54d1a08589b4",
         "created_by": "your-username",
         "dataset_id": dataset_id,
-        "service": f"service-account-{service_name}",
+        "operation": operation,
         "created_at": "2020-02-29T00:00:00+00:00",
         "expires_at": "2022-02-28T00:00:00+00:00",
         "is_active": True,
@@ -30,7 +30,7 @@ def test_create_webhook_token(requests_mock):
         additional_matcher=body_matcher,
     )
 
-    assert client.create_webhook_token(dataset_id, service_name) == res
+    assert client.create_webhook_token(dataset_id, operation) == res
 
 
 def test_list_webhook_tokens(requests_mock):
@@ -43,7 +43,7 @@ def test_list_webhook_tokens(requests_mock):
             "token": "774d8f35-fb4b-4e06-9d7f-54d1a08589b4",
             "created_by": "your-username",
             "dataset_id": dataset_id,
-            "service": "service-account-some-service",
+            "operation": "read",
             "created_at": "2020-02-29T00:00:00+00:00",
             "expires_at": "2022-02-28T00:00:00+00:00",
             "is_active": True,
@@ -80,7 +80,10 @@ def test_authorize_webhook_token(requests_mock):
     client = WebhookClient()
     dataset_id = "dataset-id"
     token = "774d8f35-fb4b-4e06-9d7f-54d1a08589b4"
-    matcher = re.compile(f"{client.api_url}/{dataset_id}/tokens/{token}/authorize")
+    operation = "write"
+    matcher = re.compile(
+        f"{client.api_url}/{dataset_id}/tokens/{token}/authorize\\?operation={operation}"
+    )
 
     res = {"access": True, "reason": None}
     requests_mock.register_uri(
@@ -90,4 +93,4 @@ def test_authorize_webhook_token(requests_mock):
         status_code=200,
     )
 
-    assert client.authorize_webhook_token(dataset_id, token) == res
+    assert client.authorize_webhook_token(dataset_id, token, operation) == res
