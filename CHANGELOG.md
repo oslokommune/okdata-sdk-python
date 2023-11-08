b701bbf976ae7d07cd87a56e16081d7b1feba8a1
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2023-11-08 08:45:31 +0100

    Bump version: 2.4.1 → 3.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## ?.?.?
+## 3.0.0
 
 * Added support for Python 3.12.
 * Dropped support for Python 3.7 which has reached end of life. Python 3.8+ is
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="okdata-sdk",
-    version="2.4.1",
+    version="3.0.0",
     author="Oslo Origo",
     author_email="dataplattform@oslo.kommune.no",
     description="SDK for origo dataplatform",
